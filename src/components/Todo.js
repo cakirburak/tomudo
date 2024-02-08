2af61d6c78c86540bc4ad7097c529534848fcb5a
cakirburak <cakirburak43@hotmail.com>
2024-02-08 21:29:03 +0300

    delete functionality is implemented

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,18 @@
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export const Todo = () => {
+export const Todo = ({ todo, toogleComplete, handleDelete }) => {
 	return (
-		<div>Todo</div>
+		<div className="Todo">
+			<p
+				className={`${todo.completed ? "completed" : "incompleted"}`}
+				onClick={() => toogleComplete(todo.id)}
+			>{todo.task}</p>
+			<div>
+				<FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
+				<FontAwesomeIcon className="delete-icon" onClick={() => handleDelete(todo.id)} icon={faTrash} />
+			</div>
+		</div>
 	)
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { TodoForm } from "./TodoForm"
 import { v4 as uuidv4 } from "uuid"
+import { Todo } from "./Todo"
 uuidv4()
 
 export const TodoWrapper = () => {
@@ -15,9 +16,27 @@ export const TodoWrapper = () => {
 			isEditing: false
 		}])
 	}
+
+	const toggleComplete = (id) => {
+		setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+	}
+
+	const handleDelete = (id) => {
+		setTodos(todos.filter(todo => todo.id !== id))
+	}
+
 	return (
 		<div className="TodoWrapper">
+			<h1>Get Things Done!</h1>
 			<TodoForm addTodo={addTodo} />
+			{todos.map((todo, index) => (
+				<Todo
+					todo={todo}
+					key={index}
+					toogleComplete={toggleComplete}
+					handleDelete={handleDelete}
+				/>
+			))}
 		</div>
 	)
 }
