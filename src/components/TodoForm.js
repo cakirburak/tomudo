ebb6cac613a15a06d5b1e9a477cd0ea7858da0a3
cakirburak <cakirburak43@hotmail.com>
2024-02-08 20:40:51 +0300

    todo form is implemented

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,25 @@
+import { useState } from "react"
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
+
+	const [value, setValue] = useState("")
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		addTodo(value);
+		setValue("")
+	}
+
 	return (
-		<div>TodoForm</div>
+		<form className="TodoForm" onSubmit={handleSubmit}>
+			<input
+				type="text"
+				className="todo-input"
+				placeholder="What is the task today?"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<button type="submit" className="todo-btn">Add Task</button>
+		</form>
 	)
 }
